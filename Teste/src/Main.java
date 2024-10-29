c80564fbce328b5e26c8b6642667821cc0d0c509
Leonardo Amichi <leoamichi1801@gmail.com>
2024-10-28 22:08:44 -0300

    .

--- Teste/src/Main.java
+++ Teste/src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello oi");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
