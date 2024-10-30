757521d3ad681bbc9ac68a7049dc971e98e415dc
Leonardo Amichi <leoamichi1801@gmail.com>
2024-10-30 11:59:47 -0300

    Aula01

--- Teste/src/Main.java
+++ Teste/src/Main.java
@@ -1,10 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello ");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        int x = 10;
+        System.out.printf("Soma: %d", x + 10);
     }
 }
