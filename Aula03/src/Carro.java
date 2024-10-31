4159f00917d802e477bfe0e7a692e15aa0b74f4d
Leonardo Amichi <164438117+LeoAmichi@users.noreply.github.com>
2024-10-31 10:33:58 -0300

    Add files via upload

---
+++ Aula03/src/Carro.java
@@ -0,0 +1,11 @@
+public class Carro {
+    String nome;
+    String marca;
+    int idade;
+
+     void MostrarInfo(){
+        System.out.printf("Nome: %s", nome);
+        System.out.printf("%nMarca: %s", marca);
+        System.out.printf("%nIdade: %d", idade);
+    }
+}
---
+++ Aula03/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Carro modelo = new Carro();
+
+        System.out.print("Digite o nome do carro: ");
+        modelo.nome = scan.next();
+
+        System.out.print("Digite a marca do carro: ");
+        modelo.marca = scan.next();
+
+        System.out.print("Digite o ano do carro: ");
+        modelo.idade = scan.nextInt();
+
+        modelo.MostrarInfo();
+    }
+}
